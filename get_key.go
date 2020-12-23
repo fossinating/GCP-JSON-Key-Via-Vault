1e19576136bc5cada46c9dd006d111a9330e7091
MrRiptide <mrriptidemc@gmail.com>
2020-12-23 14:11:05 -0500

    Create get_key.go

---
+++ get_key.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"github.com/hashicorp/vault/api"
+	"os"
+//	"encoding/json"
+	"encoding/base64"
+	"io/ioutil"
+)
+
+var token = os.Getenv("VAULT_TOKEN")
+var vault_addr = os.Getenv("VAULT_ADDR")
+
+func main() {
+	config := &api.Config{
+		Address: vault_addr,
+	}
+	client, err := api.NewClient(config)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	c := client.Logical()
+	secret, err := c.Read("gcp/key/my-key-roleset")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	
+	fmt.Println("Lease ID: ", secret.LeaseID)
+	key, _ := base64.StdEncoding.DecodeString(fmt.Sprintf("%v", secret.Data["private_key_data"]))
+	ioutil.WriteFile("new_cert.json", key, 0644)
+	fmt.Println("Successfully outputted the key to new_cert.json")
+}
